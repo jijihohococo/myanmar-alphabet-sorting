63ecdfbcadf1e17767cd5d04d6af4a957295464f
suzume <johnstevemap@gmail.com>
2021-07-14 13:24:18 +0630

    fixing running testing in command line errors

--- test/index.php
+++ test/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once($_SERVER['DOCUMENT_ROOT']. '/src/Sorting.php' );
+require_once  __DIR__.'/../src/Sorting.php';
 
 use JiJiHoHoCoCo\MyanmarAlphabetSorting\Sorting;
 
@@ -25,12 +25,10 @@ $descendingSortedArray=[
 
 
 if($ascendingSortedArray==Sorting::ascendingOrder($array)){
-	echo "Testing Myanmar alphabet sorting in ascending order is passed.";
-	echo "<br>";
+	echo "Testing Myanmar alphabet sorting in ascending order is passed.\n";
 }
 
 if($descendingSortedArray==Sorting::descendingOrder($array)){
-	echo "Testing Myanmar alphabet sorting in descending order is passed.";
-	echo "<br>";
+	echo "Testing Myanmar alphabet sorting in descending order is passed.\n";
 }
 
