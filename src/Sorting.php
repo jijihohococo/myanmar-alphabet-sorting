b52f4cf30937f46b0e9154a35644011b44d993d0
suzume <johnstevemap@gmail.com>
2021-07-16 04:24:08 +0630

    add multidimensional array sorting by field name

--- src/Sorting.php
+++ src/Sorting.php
@@ -200,42 +200,113 @@ class Sorting{
 		"uAA7F"
 	];
 
-		private static function check(array $letterArray){
-			$arrayNumber=null;
-			foreach ($letterArray as $letter) {
-				$arrayNumber .= array_search($letter, self::$characters);
-			}
-			if($arrayNumber!==null){
-				return $arrayNumber;
-			}
+	private static function check(array $letterArray){
+		$arrayNumber=null;
+		foreach ($letterArray as $letter) {
+			$arrayNumber .= array_search($letter, self::$characters);
 		}
-
-		public static function ascendingOrder(array $alphabets){
-			$resultArray=self::sort($alphabets);
-			ksort($resultArray);
-			return array_values($resultArray);
+		if($arrayNumber!==null){
+			return $arrayNumber;
 		}
+	}
 
-		public static function descendingOrder(array $alphabets){
-			$resultArray=self::sort($alphabets);
-			krsort($resultArray);
-			return array_values($resultArray);
+	private static function changeMMNumber($string){
+
+		$engNumber   = ["1", "2", "3" , "4" ,"5" ,"6" ,"7" ,"8" ,"9", "0"];
+		$mmNumber = ["၁","၂","၃","၄","၅","၆","၇","၈","၉","၀"];
+		return  str_replace($engNumber, $mmNumber, $string);
+	}
+
+	private static function makeDataArray(array $multiArray,$key){
+		$dataArray=[];
+		foreach ($multiArray as $value) {
+			$dataArray[]=$value[$key];
 		}
+		return $dataArray;
+	}
 
-		private static function sort(array $alphabets) {
-			$result=[];
-			foreach($alphabets as $key => $alphabet){
-				$letterArray=explode('\\', substr(json_encode($alphabet), 1,-1) );
-				if(isset($letterArray[1])){
-					unset($letterArray[0]);
-					$arrayNumber=self::check($letterArray);
-					if($arrayNumber!==null){
-						$result["u".$arrayNumber]=$alphabet;
-					}
+	public static function ascendingMulti(array $multiArray,$key){
+		return self::multiSort($multiArray,$key,'ascendingOrder');
+	}
+
+	public static function descendingMulti(array $multiArray,$key){
+		return self::multiSort($multiArray,$key,'descendingOrder');
+	}
+
+	private static function checkUnique(array $dataArray){
+		return $dataArray!==array_unique($dataArray);
+	}
+
+	private static function multiSort(array $multiArray,$key,$method){
+		$arrayNumber=$result=$arrayCountValue=$changedArray=$originalArray=[];
+		$dataArray=self::makeDataArray($multiArray,$key);
+		
+		
+		if(self::checkUnique($dataArray) ){
+			
+			foreach(array_count_values($dataArray) as  $dKey => $value){
+				if($value>1){
+					$arrayCountValue[$dKey]=$value;
 				}
 			}
-			return $result;
-
+			foreach($arrayCountValue as $cKey => $cValue){
+				foreach(range(1,$cValue) as $rangeValue){
+					$changedData=' -'.self::changeMMNumber($rangeValue);
+					  $multiArray[array_search($cKey, array_column($multiArray, $key ))][$key]=$cKey . $changedData;
+					  $originalArray[]=$cKey;
+					  $changedArray[]=$changedData;
+				}
+			}
+			$dataArray=self::makeDataArray($multiArray,$key);
 		}
 
+		$newData=self::{$method}($dataArray);
+		
+			foreach($newData as $k => $data){
+				$arrayNumber[]=array_search($data, array_column($multiArray, $key));
+			}
+			foreach($multiArray as $dataKey => $d){
+				$multiData=$multiArray[$arrayNumber[$dataKey]];
+				if(!empty($arrayCountValue) ){
+					foreach ($changedArray as $changedKey => $changed ) {
+						if(strpos($multiData[$key], $changed )==TRUE){
+							$changedString=$originalArray[$changedKey] . $changed;
+							$multiData[$key]=substr($changedString, 0, strpos($changedString, $changed ));
+						}
+					}
+				}
+				$result[]=$multiData;
+			}
+
+			return $result;
 	}
+
+	public static function ascendingOrder(array $alphabets){
+		$resultArray=self::sort($alphabets);
+		ksort($resultArray);
+		return array_values($resultArray);
+	}
+
+	public static function descendingOrder(array $alphabets){
+		$resultArray=self::sort($alphabets);
+		krsort($resultArray);
+		return array_values($resultArray);
+	}
+
+	private static function sort(array $alphabets) {
+		$result=[];
+		foreach($alphabets as $key => $alphabet){
+			$letterArray=explode('\\', substr(json_encode($alphabet), 1,-1) );
+			if(isset($letterArray[1])){
+				unset($letterArray[0]);
+				$arrayNumber=self::check($letterArray);
+				if($arrayNumber!==null){
+					$result["u".$arrayNumber]=$alphabet;
+				}
+			}
+		}
+		return $result;
+
+	}
+
+}
--- test/index.php
+++ test/index.php
@@ -23,6 +23,47 @@ $descendingSortedArray=[
 "ကေ","ကူး","ကူ","ကု","ကီ","ကိ","ကား","ကာ","က"
 ];
 
+$multiDimensionalArray=[
+['name'=>'ကေကေ',"age"=>'12'],
+['name'=>'ကီကီ','age'=>'14'],
+['name'=>'ကူးကူး','age'=>'13']
+];
+
+$ascendingMultiDimensioanlArray=[
+['name'=>'ကီကီ','age'=>'14'],
+['name'=>'ကူးကူး','age'=>'13'],
+['name'=>'ကေကေ',"age"=>'12']
+];
+
+$descendingMultiDimensioanlArray=[
+['name'=>'ကေကေ',"age"=>'12'],
+['name'=>'ကူးကူး','age'=>'13'],
+['name'=>'ကီကီ','age'=>'14']
+];
+
+$duplicatedMultiDimensionalArray=[
+['name'=>'ကေကေ',"age"=>'12'],
+['name'=>'ကီကီ','age'=>'14'],
+['name'=>'ကူးကူး','age'=>'13'],
+['name'=>'ကီကီ','age'=>'20']
+];
+
+$ascendingDuplicatedMultiDimensionalArray=[
+['name'=>'ကီကီ','age'=>'14'],
+['name'=>'ကီကီ','age'=>'20'],
+['name'=>'ကူးကူး','age'=>'13'],
+['name'=>'ကေကေ',"age"=>'12']
+];
+
+$descendingDuplicatedMultiDimensionalArray=[
+['name'=>'ကေကေ',"age"=>'12'],
+['name'=>'ကူးကူး','age'=>'13'],
+['name'=>'ကီကီ','age'=>'20'],
+['name'=>'ကီကီ','age'=>'14']
+];
+
+
+
 
 if($ascendingSortedArray==Sorting::ascendingOrder($array)){
 	echo "Testing Myanmar alphabet sorting in ascending order is passed.\n";
@@ -30,5 +71,22 @@ if($ascendingSortedArray==Sorting::ascendingOrder($array)){
 
 if($descendingSortedArray==Sorting::descendingOrder($array)){
 	echo "Testing Myanmar alphabet sorting in descending order is passed.\n";
+}
+
+if($ascendingMultiDimensioanlArray==Sorting::ascendingMulti($multiDimensionalArray,'name')){
+echo "Testing Myanmar alphabet sorting by field name in multi dimensioanl array in ascending order is passed.\n";
+}
+
+if($descendingMultiDimensioanlArray==Sorting::descendingMulti($multiDimensionalArray,'name')){
+echo "Testing Myanmar alphabet sorting by field name in multi dimensioanl array in descending order is passed.\n";
+}
+
+if($ascendingDuplicatedMultiDimensionalArray==Sorting::ascendingMulti($duplicatedMultiDimensionalArray,'name')){
+echo "Testing Myanmar alphabet sorting by field name in duplicated value in multi dimensioanl array in ascending order is passed.\n";
+}
+
+if($descendingDuplicatedMultiDimensionalArray==Sorting::descendingMulti($duplicatedMultiDimensionalArray,'name')){
+echo "Testing Myanmar alphabet sorting by field name in duplicated value in multi dimensioanl array in descending order is passed.\n";
+
 }
 
