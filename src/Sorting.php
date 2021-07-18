68b59c02a5bdc7a418afd6446798a9909e03900b
suzume <johnstevemap@gmail.com>
2021-07-19 02:25:07 +0630

    add zawgyi sorting

--- src/Sorting.php
+++ src/Sorting.php
@@ -4,9 +4,244 @@ namespace JiJiHoHoCoCo\MyanmarAlphabetSorting;
 
 class Sorting{
 
+	private static $zawgyi=false;
+
 	private static function check(array $letterArray){
 		$arrayNumber=null;
 		foreach ($letterArray as $letter) {
+			if(self::$zawgyi==true){
+				switch($letter){
+					case 'u1033':
+					$letter="u102F";
+					break;
+
+					case "u1034":
+					$letter="u1030";
+					break;
+
+					case 'u1039':
+					$letter='u103A';
+					break;
+
+					case 'u103A':
+					$letter='u103B';
+					break;
+
+					case 'u103B':
+					$letter='u103C';
+					break;
+
+					case 'u103C':
+					$letter='u103D';
+					break;
+
+					case 'u103D':
+					$letter='u103E';
+					break;
+
+					case 'u104E':
+					$letter="u104E" . "u1004"."u1039"."u1038";
+					break;
+
+					case 'u105A':
+					$letter="u102B" . "u103A";
+					break;
+
+					case 'u1060':
+					$letter='u1039'.'u1000';
+					break;
+
+					case 'u1061':
+					$letter='u1039' . 'u1001';
+					break;
+
+					case 'u1062':
+					$letter='u1039' . 'u1002';
+					break;
+
+					case 'u1063':
+					$letter='u1039' . 'u1003';
+					break;
+
+					case 'u1064':
+					$letter='u1004'.'u103A'.'u1039';
+					break;
+
+					case 'u1065':
+					$letter='u1039' . 'u1005';
+					break;
+
+					case 'u1066':
+					case 'u1067':
+					$letter='u1039' . 'u1006';
+					break;
+
+					case 'u1068':
+					$letter='u1039' . 'u1007';
+					break;
+
+					case 'u1069':
+					$letter='u1039' . 'u1008';
+					break;
+
+					case 'u106A':
+					$letter='u1009' . 'u1039';
+					break;
+
+					case 'u106B':
+					$letter='u100A' . 'u1039';
+					break;
+
+					case 'u106C':
+					$letter='u1039' . 'u100B';
+					break;
+
+					case 'u106D':
+					$letter='u1039' . 'u100C';
+					break;
+
+					case 'u106E':
+					$letter='u100B'.'u1039'.'u100B';
+					break;
+
+					case 'u106F':
+					$letter='u100D' . 'u1039' . 'u100E';
+					break;
+
+
+					case 'u1070':
+					$letter='u1039' . 'u100F';
+					break;
+
+					case 'u1071':
+					case 'u1072':
+					$letter='u1039' . 'u1010';
+					break;
+
+					case 'u1073':
+					case 'u1074':
+					$letter='u1039' . 'u1011';
+					break;
+
+					case 'u1075':
+					$letter='u1039' . 'u1012';
+					break;
+
+					case 'u1076':
+					$letter='u1039' . 'u1013';
+					break;
+
+					case 'u1077':
+					$letter='u1039' . 'u1014';
+					break;
+
+					case 'u1078':
+					$letter='u1039' . 'u1015';
+					break;
+
+					case 'u1079':
+					$letter='u1039' . 'u1016';
+					break;
+
+					case 'u107A':
+					$letter='u1039' . 'u1017';
+					break;
+
+					case 'u107B':
+					case 'u1093':
+					$letter='u1039' . 'u1018';
+					break;
+
+					case 'u107C':
+					$letter='u1039' . 'u1019';
+					break;
+
+					case 'u107D':
+					$letter='u1039' . 'u103B';
+					break;
+
+
+					case 'u107E':
+					case 'u107F':
+					case 'u1080':
+					case 'u1081':
+					case 'u1082':
+					case 'u1083':
+					case 'u1084':
+					$letter='u103B';
+					break;
+
+					case 'u1085':
+					$letter='u1039'.'u101C';
+					break;
+
+					case 'u1086':
+					$letter="u103F";
+					break;
+
+					case 'u1087':
+					$letter='u103E';
+					break;
+
+					case 'u1088':
+					$letter='u103E'.'u102F';
+					break;
+
+					case 'u1089':
+					$letter='u103E'.'u1030';
+					break;
+
+					case 'u108A':
+					$letter="u103C" . "u103D";
+					break;
+
+					case 'u108B':
+					$letter='u1004'.'u103A'.'u1039'.'u102D';
+					break;
+
+					case 'u108C':
+					$letter='u1004'.'u103A'.'u1039'.'u102E';
+					break;
+
+					case 'u108D':
+					$letter='u1004' . 'u103A' . 'u1039' . 'u1036';
+					break;
+
+					case 'u108E':
+					$letter='u102D' . 'u1036';
+					break;
+
+					case 'u108F':
+					$letter='u1014'. 'u1039';
+					break;
+
+					case 'u1090':
+					$letter='u101B' . 'u1039';
+					break;
+
+					case 'u1091':
+					$letter='u100F'. 'u1039' . 'u100D';
+					break;
+
+					case 'u1092':
+					$letter='u100B' . 'u1039' . 'u100C';
+					break;
+
+					case 'u1094':
+					case 'u1095':
+					$letter='u1037';
+					break;
+
+					case 'u1096':
+					$letter='u1039' . 'u1010' . 'u103D';
+					break;
+
+					case 'u1097':
+					$letter='u100B' . 'u1039';
+					break;
+
+				}
+			}
 			$arrayNumber .= $letter;
 		}
 		if($arrayNumber!==null){
@@ -27,6 +262,14 @@ class Sorting{
 			$dataArray[]=$value[$key];
 		}
 		return $dataArray;
+	}
+
+	public static function ascendingMultiZawgyi(array $multiArray,$key){
+		return self::multiSort($multiArray,$key,'ascendingOrderZawgyi');
+	}
+
+	public static function descendingMultiZawgyi(array $multiArray,$key){
+		return self::multiSort($multiArray,$key,'descendingOrderZawgyi');
 	}
 
 	public static function ascendingMulti(array $multiArray,$key){
@@ -83,6 +326,20 @@ class Sorting{
 		}
 
 		return $result;
+	}
+
+	public static function ascendingOrderZawgyi(array $alphabets){
+		self::$zawgyi=true;
+		$resultArray=self::sort($alphabets);
+		ksort($resultArray);
+		return array_values($resultArray);
+	}
+
+	public static function descendingOrderZawgyi(array $alphabets){
+		self::$zawgyi=true;
+		$resultArray=self::sort($alphabets);
+		krsort($resultArray);
+		return array_values($resultArray);
 	}
 
 	public static function ascendingOrder(array $alphabets){
--- test/index.php
+++ test/index.php
@@ -9,6 +9,7 @@ $array=[ "က",
 "ကိ",
 "ကီ",
 "ကေ",
+"ကတ်",
 "ကု",
 "ကူ",
 "ကူး",
@@ -19,76 +20,86 @@ $array=[ "က",
 $ascendingSortedArray=[
 "က","က","ကာ","ကား","ကိ","ကီ","ကု","ကူ","ကူး","ကေ"
 ];
+print_r(Sorting::ascendingOrderZawgyi(["က",
+"ကား",
+"ကိ",
+"ကီ",
+"ေက",
+"ကု",
+"ကူ",
+"ကူး",
+"က",
+"ကတ္",
+"ကာ"]));
+
+// $descendingSortedArray=[
+// "ကေ","ကူး","ကူ","ကု","ကီ","ကိ","ကား","ကာ","က","က"
+// ];
+
+// $multiDimensionalArray=[
+// ['name'=>'ကေကေ',"age"=>'12'],
+// ['name'=>'ကီကီ','age'=>'14'],
+// ['name'=>'ကူးကူး','age'=>'13']
+// ];
+
+// $ascendingMultiDimensioanlArray=[
+// ['name'=>'ကီကီ','age'=>'14'],
+// ['name'=>'ကူးကူး','age'=>'13'],
+// ['name'=>'ကေကေ',"age"=>'12']
+// ];
+
+// $descendingMultiDimensioanlArray=[
+// ['name'=>'ကေကေ',"age"=>'12'],
+// ['name'=>'ကူးကူး','age'=>'13'],
+// ['name'=>'ကီကီ','age'=>'14']
+// ];
+
+// $duplicatedMultiDimensionalArray=[
+// ['name'=>'ကေကေ',"age"=>'12'],
+// ['name'=>'ကီကီ','age'=>'14'],
+// ['name'=>'ကူးကူး','age'=>'13'],
+// ['name'=>'ကီကီ','age'=>'20']
+// ];
+
+// $ascendingDuplicatedMultiDimensionalArray=[
+// ['name'=>'ကီကီ','age'=>'14'],
+// ['name'=>'ကီကီ','age'=>'20'],
+// ['name'=>'ကူးကူး','age'=>'13'],
+// ['name'=>'ကေကေ',"age"=>'12']
+// ];
+
+// $descendingDuplicatedMultiDimensionalArray=[
+// ['name'=>'ကေကေ',"age"=>'12'],
+// ['name'=>'ကူးကူး','age'=>'13'],
+// ['name'=>'ကီကီ','age'=>'20'],
+// ['name'=>'ကီကီ','age'=>'14']
+// ];
 
 
-$descendingSortedArray=[
-"ကေ","ကူး","ကူ","ကု","ကီ","ကိ","ကား","ကာ","က","က"
-];
-
-$multiDimensionalArray=[
-['name'=>'ကေကေ',"age"=>'12'],
-['name'=>'ကီကီ','age'=>'14'],
-['name'=>'ကူးကူး','age'=>'13']
-];
-
-$ascendingMultiDimensioanlArray=[
-['name'=>'ကီကီ','age'=>'14'],
-['name'=>'ကူးကူး','age'=>'13'],
-['name'=>'ကေကေ',"age"=>'12']
-];
-
-$descendingMultiDimensioanlArray=[
-['name'=>'ကေကေ',"age"=>'12'],
-['name'=>'ကူးကူး','age'=>'13'],
-['name'=>'ကီကီ','age'=>'14']
-];
-
-$duplicatedMultiDimensionalArray=[
-['name'=>'ကေကေ',"age"=>'12'],
-['name'=>'ကီကီ','age'=>'14'],
-['name'=>'ကူးကူး','age'=>'13'],
-['name'=>'ကီကီ','age'=>'20']
-];
-
-$ascendingDuplicatedMultiDimensionalArray=[
-['name'=>'ကီကီ','age'=>'14'],
-['name'=>'ကီကီ','age'=>'20'],
-['name'=>'ကူးကူး','age'=>'13'],
-['name'=>'ကေကေ',"age"=>'12']
-];
-
-$descendingDuplicatedMultiDimensionalArray=[
-['name'=>'ကေကေ',"age"=>'12'],
-['name'=>'ကူးကူး','age'=>'13'],
-['name'=>'ကီကီ','age'=>'20'],
-['name'=>'ကီကီ','age'=>'14']
-];
 
 
+// if($ascendingSortedArray==Sorting::ascendingOrder($array)){
+// 	echo "Testing Myanmar alphabet sorting in ascending order is passed.\n";
+// }
 
+// if($descendingSortedArray==Sorting::descendingOrder($array)){
+// 	echo "Testing Myanmar alphabet sorting in descending order is passed.\n";
+// }
 
-if($ascendingSortedArray==Sorting::ascendingOrder($array)){
-	echo "Testing Myanmar alphabet sorting in ascending order is passed.\n";
-}
+// if($ascendingMultiDimensioanlArray==Sorting::ascendingMulti($multiDimensionalArray,'name')){
+// echo "Testing Myanmar alphabet sorting by field name in multi dimensioanl array in ascending order is passed.\n";
+// }
 
-if($descendingSortedArray==Sorting::descendingOrder($array)){
-	echo "Testing Myanmar alphabet sorting in descending order is passed.\n";
-}
+// if($descendingMultiDimensioanlArray==Sorting::descendingMulti($multiDimensionalArray,'name')){
+// echo "Testing Myanmar alphabet sorting by field name in multi dimensioanl array in descending order is passed.\n";
+// }
 
-if($ascendingMultiDimensioanlArray==Sorting::ascendingMulti($multiDimensionalArray,'name')){
-echo "Testing Myanmar alphabet sorting by field name in multi dimensioanl array in ascending order is passed.\n";
-}
+// if($ascendingDuplicatedMultiDimensionalArray==Sorting::ascendingMulti($duplicatedMultiDimensionalArray,'name')){
+// echo "Testing Myanmar alphabet sorting by field name in duplicated value in multi dimensioanl array in ascending order is passed.\n";
+// }
 
-if($descendingMultiDimensioanlArray==Sorting::descendingMulti($multiDimensionalArray,'name')){
-echo "Testing Myanmar alphabet sorting by field name in multi dimensioanl array in descending order is passed.\n";
-}
+// if($descendingDuplicatedMultiDimensionalArray==Sorting::descendingMulti($duplicatedMultiDimensionalArray,'name')){
+// echo "Testing Myanmar alphabet sorting by field name in duplicated value in multi dimensioanl array in descending order is passed.\n";
 
-if($ascendingDuplicatedMultiDimensionalArray==Sorting::ascendingMulti($duplicatedMultiDimensionalArray,'name')){
-echo "Testing Myanmar alphabet sorting by field name in duplicated value in multi dimensioanl array in ascending order is passed.\n";
-}
-
-if($descendingDuplicatedMultiDimensionalArray==Sorting::descendingMulti($duplicatedMultiDimensionalArray,'name')){
-echo "Testing Myanmar alphabet sorting by field name in duplicated value in multi dimensioanl array in descending order is passed.\n";
-
-}
+// }
 
